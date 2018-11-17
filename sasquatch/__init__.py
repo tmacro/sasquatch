266532ec4e682437f95f636b497c40025f487d90
Taylor McKinnon <mail@tmacs.space>
2018-11-16 23:38:05 -0800

    Change default logging level to INFO, and add global except

--- sasquatch/__init__.py
+++ sasquatch/__init__.py
@@ -1,7 +1,9 @@
+import logging
+
 from .util.log import setupLogging
 
 __version__ = '0.0.0'
 
-setupLogging('sasquatch', __version__)
+setupLogging('sasquatch', __version__, loglvl=logging.INFO)
 
 from . import entry
