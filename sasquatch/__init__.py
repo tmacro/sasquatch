11675f13db19b6e44010534e8dc88b32ac7b9d19
Taylor McKinnon <mail@tmacs.space>
2018-11-16 23:25:27 -0800

    Change default logging level to INFO

--- sasquatch/__init__.py
+++ sasquatch/__init__.py
@@ -1,7 +1,8 @@
 from .util.log import setupLogging
+import logging
 
 __version__ = '0.0.0'
 
-setupLogging('sasquatch', __version__)
+setupLogging('sasquatch', __version__, loglvl=logging.INFO)
 
 from . import entry
